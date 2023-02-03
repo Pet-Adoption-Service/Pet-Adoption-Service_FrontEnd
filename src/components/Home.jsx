7b43b19f9fb2554bb290dd2254e26e9b0d37f0a4
Berni Wong <3colouredmochi@gmail.com>
2023-02-03 14:06:31 +1100

    Nav Bars are done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Welcome home to a new furry friend!</div>
+    <div></div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMediaQuery} from 'react-responsive'
+import { Input, Menu } from 'semantic-ui-react'
 import NavbarLg from './NavbarLg'
+import NavbarMb from './NavbarMb'
 
 const Navbar = () => {
+  const [activeItem, setActiveItem] = useState('home')
+  const handleItemClick = (e, { name }) => setActiveItem(name)
+  const renderLinks=()=>{
+
+    return <>
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='about'
+        active={activeItem === 'about'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='find a pet'
+        active={activeItem === 'find a pet'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='contact'
+        active={activeItem === 'contact'}
+        onClick={handleItemClick}
+        />
+      <Menu.Item
+        name='FAQ'
+        active={activeItem === 'faq'}
+        onClick={handleItemClick}
+      />
+      <Menu.Menu position='right'>
+        <Menu.Item>
+          <Menu Secondary attached size="mini">
+          <Input icon='search' placeholder='Search...' />
+          </Menu>
+        </Menu.Item>
+      <Menu.Item
+        name='login'
+        active={activeItem === 'login'}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        name='register'
+        active={activeItem === 'register'}
+        onClick={handleItemClick}
+      />  
+      </Menu.Menu>
+    </>
+}
+
+  const none = useMediaQuery({ query: '(max-width: 576px)' })
+  const sm = useMediaQuery({ query: '(min-width: 576px)' })
+  const md = useMediaQuery({ query: '(min-width: 768px)' })
+  const lg = useMediaQuery({ query: '(min-width: 992px)' })
+  const xl = useMediaQuery({ query: '(min-width: 1200px)' })
+  const xxl = useMediaQuery({ query: '(min-width: 1400px)' })
+    const size = {none, sm, md, lg, xl, xxl}
   return (
     <div>
-      <NavbarLg />
+      {size.sm ? <NavbarLg renderLinks={renderLinks}/> : <NavbarMb renderLinks={renderLinks}/>}
     </div>
   )
 }
