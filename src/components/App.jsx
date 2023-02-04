574713fa6bf52c65cdc074c3b439f1c17a25794d
Berni Wong <3colouredmochi@gmail.com>
2023-02-04 21:49:17 +1100

    Fixed the NavBar I want to cry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
-import FindAPet from './FindAPet'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import FAQ from './FAQ'
-import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 const App = () => {
   return (
     <>
-        <h1>The Pet Adoption Service</h1>
-        <Navbar />
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/Find/Pet" element={<FindAPet />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/FAQ" element={<FAQ />} />
-                <Route path="*" element={<h4>Page not found!</h4>} />
-            </Routes>
-        </BrowserRouter>
-        {/* <Home />}
-        <About />
-        <FindAPet />
-        <Contact />
-        <FAQ /> */}
+      <Navbar />
     </>    
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,31 @@
-import React, { useState } from 'react'
-import { useMediaQuery} from 'react-responsive'
-import { Input, Menu } from 'semantic-ui-react'
-import NavbarLg from './NavbarLg'
-import NavbarMb from './NavbarMb'
-
-const Navbar = () => {
-  const [activeItem, setActiveItem] = useState('home')
-  const handleItemClick = (e, { name }) => setActiveItem(name)
-  const renderLinks=()=>{
-
-    return <>
-      <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name='about'
-        active={activeItem === 'about'}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name='find a pet'
-        active={activeItem === 'find a pet'}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name='contact'
-        active={activeItem === 'contact'}
-        onClick={handleItemClick}
-        />
-      <Menu.Item
-        name='FAQ'
-        active={activeItem === 'faq'}
-        onClick={handleItemClick}
-      />
-      <Menu.Menu position='right'>
-        <Menu.Item>
-          <Menu Secondary attached size="mini">
-          <Input icon='search' placeholder='Search...' />
-          </Menu>
-        </Menu.Item>
-      <Menu.Item
-        name='login'
-        active={activeItem === 'login'}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name='register'
-        active={activeItem === 'register'}
-        onClick={handleItemClick}
-      />  
-      </Menu.Menu>
-    </>
+import React, { Component } from 'react';
+class NavBarUIMenu extends Component {
+    render() {
+        return (
+            <div className="ui container">
+                <h2 style={{ color: 'green' }}>The Pet Adoption Service</h2>
+                <h3>Become a pet owner today!</h3>
+                <div className="ui secondary menu">
+                    <a href="./home" className="item active">
+                        Home
+                    </a>
+                    <a href="./about" className="item">
+                        About
+                    </a>
+                    <a href="./FindAPet" className="item">
+                        Find A Pet
+                    </a>
+                    <a href="./Contact" className="item">
+                        Contact
+                    </a>
+                    <a href="./FAQ" className="item"> 
+                        FAQ 
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
 
-  const none = useMediaQuery({ query: '(max-width: 576px)' })
-  const sm = useMediaQuery({ query: '(min-width: 576px)' })
-  const md = useMediaQuery({ query: '(min-width: 768px)' })
-  const lg = useMediaQuery({ query: '(min-width: 992px)' })
-  const xl = useMediaQuery({ query: '(min-width: 1200px)' })
-  const xxl = useMediaQuery({ query: '(min-width: 1400px)' })
-    const size = {none, sm, md, lg, xl, xxl}
-  return (
-    <div>
-      {size.sm ? <NavbarLg renderLinks={renderLinks}/> : <NavbarMb renderLinks={renderLinks}/>}
-    </div>
-  )
-}
+export default NavBarUIMenu;
 
-export default Navbar
