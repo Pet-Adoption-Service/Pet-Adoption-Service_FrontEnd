d10aeff73f6a761679563eedc2df4fdda2df7695
meatsheild <dcoorloff@gmail.com>
2023-02-06 02:15:05 +1100

    ...

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -4,26 +4,13 @@ import { Link } from 'react-router-dom'
 
 const PetCard = ({ pets }) => {
   return (
-    <div class="card">
-    {/* {pets.map((pet, index) => (
-      <p key={index}>
-       <Link to ={`pet/${index}`}>{pet.name}</Link>
-       </p>
-      ))} */}
-      <img src="http://placekitten.com/100" class="img-thumbnail" alt="Pet Image" />
-      <div class="card-body text-center py-4">
-          <h5>(Pet Name)</h5>
-          <p>(Pet Info)</p>
-      </div>
-      <ul class="list-group list-group-flush">
-          <li class="list-group-item ">Breed</li>
-          <li class="list-group-item">Age</li>
-          <li class="list-group-item">Size</li>
-      </ul>
-      <div class="card-body">
-          <a href="#" class="btn btn-info">Book A Visit!</a>
-      </div>
-  </div>  )
+    <div className="col-4">
+    <img className="kitty1" src={kitty1}/>
+        <h5>Name: {pets[0].name}</h5>
+        <p>Description: {pets[0].about}</p>
+        <p>Breed:{pets[0].breed}</p>
+        <p>Age:{pets[0].age}</p>
+        </div>
 }
 
 export default PetCard
