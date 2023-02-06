a3060ee19db6c266d5467b88cf6cb370a5fb9e4e
meatsheild <dcoorloff@gmail.com>
2023-02-06 17:44:43 +1100

    images in public

--- src/components/FindAPet.jsx
+++ src/components/FindAPet.jsx
@@ -10,20 +10,18 @@ const adult2 = new URL("./images/adult2.jpg", import.meta.url)
 
 const FindAPet = ({pets}) => {
 
-    // const {pets} = useLoaderData()
 return <>
     <section className="pet-card">
         <div className="container">
             <div className="row">
                 <div className="col-4">
-                <div className="col-4">
                     <img className="kitty1" src={kitty1}/>
-                    <h5>Name: {pets[0].name}</h5>
-                    <p>Description: {pets[0].about}</p>
-                    <p>Breed: {pets[0].breed}</p>
-                    <p>Age: {pets[0].age}</p>
-                    </div>
-                    </div>
+                        <h5> {pets[0].name}</h5>
+                        <p>Description:</p>
+                        <p>Breed:</p>
+                        <p>Age:</p>
+                        <p>Size:</p>
+                        </div>
                 <div className="col-4">
                     <img className="kitty2" src={kitty2}/>
                         <h5>{pets[1].name}</h5>
@@ -68,7 +66,7 @@ return <>
                         </div>
                 </section>
             </>
-};
+}
 
-export default FindAPet;
+export default FindAPet
 
